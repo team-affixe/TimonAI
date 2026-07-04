531b5f0ad7c7553ed84cb2fb814a6e4fd2950e19
team-affixe <team.affixe-7v@icloud.com>
2026-07-04 12:56:20 +0200

    Redesigned Button

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-var input = document.getElementById("myInput");
+// var input = document.getElementById("SendButton");
 
-// Execute a function when the user presses a key on the keyboard
-input.addEventListener("keypress", function (event) {
-  // If the user presses the "Enter" key on the keyboard
-  if (event.key === "Enter") {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("myBtn").click();
-  }
-});
+// // Execute a function when the user presses a key on the keyboard
+// input.addEventListener("keypress", function (event) {
+//   // If the user presses the "Enter" key on the keyboard
+//   if (event.key === "Enter") {
+//     // Cancel the default action, if needed
+//     event.preventDefault();
+//     // Trigger the button element with a click
+//     document.getElementById("SendButton").click();
+//   }
+// });
+
+function sendMessage() {
+
+}
